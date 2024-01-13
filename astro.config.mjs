bc8c705155264d0f61ab16990612ed93554a2faf
John Allen <jallen9674@gmail.com>
2024-01-13 10:11:38 -0600

    Photo and homepage updates, icon setup updates, misc fixes and updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import vue from '@astrojs/vue'
 
-import vue from "@astrojs/vue";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), compress(), vue()]
-});
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    vue(),
+    icon(),
+  ],
+})
